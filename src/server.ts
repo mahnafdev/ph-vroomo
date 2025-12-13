966b0d1345cb06fb5dda9903d375e175784e1fd4
mahnafdev <mahnafdev@gmail.com>
2025-12-14 03:58:22 +0600

    feat(api): create GET /

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { createTables } from "./config/db";
 
 const app = express();
@@ -9,6 +9,15 @@ app.use(express.json());
 
 //* Create DB Tables
 createTables();
+
+//* GET /
+app.get("/", (req: Request, res: Response) => {
+	// Send 200 response
+	res.status(200).json({
+		success: true,
+		message: "Entered into Vroomo",
+	});
+});
 
 //* Listen server
 app.listen(port, () => {
