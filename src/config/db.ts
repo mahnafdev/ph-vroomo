06e70b784e282f107c8746872284cfd1b999d186
mahnafdev <mahnafdev@gmail.com>
2025-12-14 03:50:16 +0600

    chore(config): configure db and create db tables

---
+++ src/config/db.ts
@@ -0,0 +1,44 @@
+import { Pool } from "pg";
+import config from ".";
+
+//* DB Pool
+const db = new Pool({
+	connectionString: config.dbConnection,
+});
+
+//* Create Tables
+const createTables = async () => {
+	await db.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(150) UNIQUE NOT NULL,
+            password TEXT NOT NULL CHECK (CHAR_LENGTH(password) > 5),
+            phone VARCHAR(20) NOT NULL,
+            role VARCHAR(15) DEFAULT 'customer' CHECK (role IN ('admin', 'customer'))
+        );
+        `);
+	await db.query(`
+        CREATE TABLE IF NOT EXISTS vehicles (
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(100) NOT NULL,
+            type VARCHAR(10) CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+            registration_number VARCHAR(20) UNIQUE NOT NULL,
+            daily_rent_price INT NOT NULL CHECK (daily_rent_price >= 0),
+            availability_status VARCHAR(15) DEFAULT 'available' CHECK (availability_status IN ('available', 'booked'))
+        );
+        `);
+	await db.query(`
+        CREATE TABLE IF NOT EXISTS bookings (
+            id SERIAL PRIMARY KEY,
+            customer_id INT REFERENCES users(id),
+            vehicle_id INT REFERENCES vehicles(id),
+            rent_start_date DATE NOT NULL,
+            rent_end_date DATE NOT NULL,
+            total_price INT NOT NULL CHECK (total_price >= 0),
+            status VARCHAR(15) DEFAULT 'active' CHECK (status IN ('active', 'cancelled', 'returned'))
+        );
+        `);
+};
+
+export { db, createTables };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { createTables } from "./config/db";
 
 const app = express();
 const port = 8080;
 
 //* Global Middlewares
 app.use(express.json());
+
+//* Create DB Tables
+createTables();
 
 //* Listen server
 app.listen(port, () => {
