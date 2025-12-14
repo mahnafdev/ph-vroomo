ad7cc4642dd3e435b84e35f572412a175cc7e714
mahnafdev <mahnafdev@gmail.com>
2025-12-14 18:33:14 +0600

    feat: create auth middleware and secure GET /users

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { db } from "../database";
+
+const auth = (...roles: ("admin" | "customer")[]) => {
+	return async (req: Request, _res: Response, next: NextFunction) => {
+		// Get authorization token
+		const token = req.headers.authorization;
+		// If no token
+		if (!token) throw new Error("User not authorized");
+		// Decode token to obtain payload
+		const decoded = jwt.verify(token, config.jwtSecret) as JwtPayload;
+		// Get user
+		const user = await db.query("SELECT * FROM users WHERE email = $1", [decoded.email]);
+		// If invalid token
+		if (user.rowCount === 0) throw new Error("Invalid token");
+		// Update user
+		req.user = decoded;
+		// If action forbidden
+		if (roles.length && !roles.includes(decoded.role))
+			throw new Error("User don't have this permission");
+		next();
+	};
+};
+
+export default auth;
--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usersController from "./users.controller";
+import auth from "../../middlewares/auth";
+import { roles } from "../auth/auth.constant";
 
 const router = Router();
 
-router.get("/", usersController.getAll);
+router.get("/", auth(roles.admin as "admin" | "customer"), usersController.getAll);
 
 const usersRoutes = router;
 
